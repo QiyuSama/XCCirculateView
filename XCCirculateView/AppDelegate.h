553b5c3db30c8dbd63e92b72fb6aea71449edf92
xiangchao <455877644@qq.com>
2016-04-28 17:11:03 +0800

    Initial Commit

---
+++ XCCirculateView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCCirculateView
+//
+//  Created by xiangchao on 16/4/28.
+//  Copyright © 2016年 STV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
